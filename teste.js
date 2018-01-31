6a3fcad7b1f9efba43e4d34e3ce1f7c3b9853358
Ubuntu <ubuntu@ip-172-26-59-100.sa-east-1.compute.internal>
2018-01-31 11:43:04 +0000

    add testing

--- teste.js
+++ teste.js
@@ -1,1 +1,16 @@
-dasdd
+var chai = require('chai')
+var request = require('supertest')
+var should = chai.should()
+var server = require('./main')
+
+describe('GET /', function () {
+  it('returns 200 status code', function (done) {
+    request('http://localhost:8000').get('/')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function (err, res) {
+        done(err)
+      })
+  })
+})
+
